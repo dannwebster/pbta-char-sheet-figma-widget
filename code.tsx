00299254f2792f0b269325bd8ea4f5be1fa8509a
Dann Webster <dannwebster@gmail.com>
2025-09-29 22:24:37 -0700

    added move buttons for basic moves

--- code.tsx
+++ code.tsx
@@ -1480,9 +1480,31 @@ function pbta_character() {
                   </Text>
                   {movesData.AttributeMoves[attribute].map((move, idx) => (
                     <AutoLayout key={idx} direction="vertical" spacing={6} width="fill-parent">
-                      <Text fontSize={22} fontWeight={700} width="fill-parent">
-                        {move.name}
-                      </Text>
+                      <AutoLayout direction="horizontal" spacing={8} width="fill-parent" verticalAlignItems="center">
+                        <Text fontSize={22} fontWeight={700} width="fill-parent">
+                          {move.name}
+                        </Text>
+                        <AutoLayout
+                            fill="#333333"
+                            padding={6}
+                            cornerRadius={4}
+                            onClick={() => {
+                              roll(attributeValues[attribute], "+" + attribute, move)
+                            }}
+                        >
+                          <Frame width={18} height={18} fill="#FFFFFF" cornerRadius={3}>
+                            <AutoLayout
+                                horizontalAlignItems="center"
+                                verticalAlignItems="center"
+                                width={18}
+                                height={18}
+                                padding={4}
+                            >
+                              <Grid sides={6} size={3} fill="#333333" spacing={2} />
+                            </AutoLayout>
+                          </Frame>
+                        </AutoLayout>
+                      </AutoLayout>
                       <Text fontSize={19} width="fill-parent">
                         {move.description}
                       </Text>
@@ -1567,30 +1589,55 @@ function pbta_character() {
                 <Text fontSize={27} fontWeight={700} width="fill-parent" horizontalAlignText="center">
                   Global Moves
                 </Text>
-                {movesData.MultiAttributeMoves.find(section => section.Name === "Global Moves")?.Moves.map((move, idx) => (
-                  <AutoLayout key={idx} direction="vertical" spacing={6} width="fill-parent">
-                    <Text fontSize={22} fontWeight={700} width="fill-parent">
-                      {move.name}
-                    </Text>
-                    <Text fontSize={19} width="fill-parent">
-                      {move.description}
-                    </Text>
-                    {move.outcomes && Object.entries(move.outcomes).map(([key, value]) => (
-                      <Text key={key} fontSize={19} width="fill-parent">
-                        • On {key}: {value}
-                      </Text>
-                    ))}
-                    {move.hold && move.hold.length > 0 && (
-                      <AutoLayout direction="vertical" spacing={3} width="fill-parent">
-                        {move.hold.map((option, optIdx) => (
-                          <Text key={optIdx} fontSize={19} width="fill-parent">
-                            • {option}
-                          </Text>
-                        ))}
+                {movesData.MultiAttributeMoves.find(section => section.Name === "Global Moves")?.Moves.map((move, idx) => {
+                  const section = movesData.MultiAttributeMoves.find(s => s.Name === "Global Moves")
+                  return (
+                    <AutoLayout key={idx} direction="vertical" spacing={6} width="fill-parent">
+                      <AutoLayout direction="horizontal" spacing={8} width="fill-parent" verticalAlignItems="center">
+                        <Text fontSize={22} fontWeight={700} width="fill-parent">
+                          {move.name}
+                        </Text>
+                        <AutoLayout
+                            fill="#333333"
+                            padding={6}
+                            cornerRadius={4}
+                            onClick={() => {
+                              setPendingMultiAttributeMove({ move: move, attributes: section.Attributes })
+                            }}
+                        >
+                          <Frame width={18} height={18} fill="#FFFFFF" cornerRadius={3}>
+                            <AutoLayout
+                                horizontalAlignItems="center"
+                                verticalAlignItems="center"
+                                width={18}
+                                height={18}
+                                padding={4}
+                            >
+                              <Grid sides={6} size={3} fill="#333333" spacing={2} />
+                            </AutoLayout>
+                          </Frame>
+                        </AutoLayout>
                       </AutoLayout>
-                    )}
-                  </AutoLayout>
-                ))}
+                      <Text fontSize={19} width="fill-parent">
+                        {move.description}
+                      </Text>
+                      {move.outcomes && Object.entries(move.outcomes).map(([key, value]) => (
+                        <Text key={key} fontSize={19} width="fill-parent">
+                          • On {key}: {value}
+                        </Text>
+                      ))}
+                      {move.hold && move.hold.length > 0 && (
+                        <AutoLayout direction="vertical" spacing={3} width="fill-parent">
+                          {move.hold.map((option, optIdx) => (
+                            <Text key={optIdx} fontSize={19} width="fill-parent">
+                              • {option}
+                            </Text>
+                          ))}
+                        </AutoLayout>
+                      )}
+                    </AutoLayout>
+                  )
+                })}
               </AutoLayout>
 
               {/* Mythos/Logos Moves */}
@@ -1607,30 +1654,55 @@ function pbta_character() {
                 <Text fontSize={27} fontWeight={700} width="fill-parent" horizontalAlignText="center">
                   Mythos/Logos Moves
                 </Text>
-                {movesData.MultiAttributeMoves.find(section => section.Name === "Mythos/Logos Moves")?.Moves.map((move, idx) => (
-                  <AutoLayout key={idx} direction="vertical" spacing={6} width="fill-parent">
-                    <Text fontSize={22} fontWeight={700} width="fill-parent">
-                      {move.name}
-                    </Text>
-                    <Text fontSize={19} width="fill-parent">
-                      {move.description}
-                    </Text>
-                    {move.outcomes && Object.entries(move.outcomes).map(([key, value]) => (
-                      <Text key={key} fontSize={19} width="fill-parent">
-                        • On {key}: {value}
-                      </Text>
-                    ))}
-                    {move.hold && move.hold.length > 0 && (
-                      <AutoLayout direction="vertical" spacing={3} width="fill-parent">
-                        {move.hold.map((option, optIdx) => (
-                          <Text key={optIdx} fontSize={19} width="fill-parent">
-                            • {option}
-                          </Text>
-                        ))}
+                {movesData.MultiAttributeMoves.find(section => section.Name === "Mythos/Logos Moves")?.Moves.map((move, idx) => {
+                  const section = movesData.MultiAttributeMoves.find(s => s.Name === "Mythos/Logos Moves")
+                  return (
+                    <AutoLayout key={idx} direction="vertical" spacing={6} width="fill-parent">
+                      <AutoLayout direction="horizontal" spacing={8} width="fill-parent" verticalAlignItems="center">
+                        <Text fontSize={22} fontWeight={700} width="fill-parent">
+                          {move.name}
+                        </Text>
+                        <AutoLayout
+                            fill="#333333"
+                            padding={6}
+                            cornerRadius={4}
+                            onClick={() => {
+                              setPendingMultiAttributeMove({ move: move, attributes: section.Attributes })
+                            }}
+                        >
+                          <Frame width={18} height={18} fill="#FFFFFF" cornerRadius={3}>
+                            <AutoLayout
+                                horizontalAlignItems="center"
+                                verticalAlignItems="center"
+                                width={18}
+                                height={18}
+                                padding={4}
+                            >
+                              <Grid sides={6} size={3} fill="#333333" spacing={2} />
+                            </AutoLayout>
+                          </Frame>
+                        </AutoLayout>
                       </AutoLayout>
-                    )}
-                  </AutoLayout>
-                ))}
+                      <Text fontSize={19} width="fill-parent">
+                        {move.description}
+                      </Text>
+                      {move.outcomes && Object.entries(move.outcomes).map(([key, value]) => (
+                        <Text key={key} fontSize={19} width="fill-parent">
+                          • On {key}: {value}
+                        </Text>
+                      ))}
+                      {move.hold && move.hold.length > 0 && (
+                        <AutoLayout direction="vertical" spacing={3} width="fill-parent">
+                          {move.hold.map((option, optIdx) => (
+                            <Text key={optIdx} fontSize={19} width="fill-parent">
+                              • {option}
+                            </Text>
+                          ))}
+                        </AutoLayout>
+                      )}
+                    </AutoLayout>
+                  )
+                })}
               </AutoLayout>
 
               {/* Clock Moves */}
@@ -1648,10 +1720,20 @@ function pbta_character() {
                   Clock Moves
                 </Text>
                 {movesData.ClockMoves.map((move, idx) => (
-                  <AutoLayout key={idx} direction="vertical" spacing={6} width="fill-parent">
+                  <AutoLayout key={idx} direction="horizontal" spacing={8} width="fill-parent" verticalAlignItems="center">
                     <Text fontSize={22} fontWeight={700} width="fill-parent">
                       {move.name}
                     </Text>
+                    <AutoLayout
+                        fill="#333333"
+                        padding={6}
+                        cornerRadius={4}
+                        onClick={() => {
+                          handleClockMove(move.clock, move.action, move.name)
+                        }}
+                    >
+                      <Text fontSize={18} fontWeight={700} fill="#FFFFFF">⏱</Text>
+                    </AutoLayout>
                   </AutoLayout>
                 ))}
               </AutoLayout>
