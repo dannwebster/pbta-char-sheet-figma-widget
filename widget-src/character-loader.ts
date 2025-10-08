6677230004b546f4c318d96c7662a94816fa3140
Dann Webster <dannwebster@gmail.com>
2025-10-08 00:16:04 -0700

    cleaned up contacts data, and added contacts data for Silas

--- widget-src/character-loader.ts
+++ widget-src/character-loader.ts
@@ -1,8 +1,8 @@
 // Import all character files
-import dakota from './dakota.json'
-import silas from './silas.json'
-import jake from './jake.json'
-import example from './example-characters.json'
+import dakota from './characters/dakota.json'
+import silas from './characters/silas.json'
+import jake from './characters/jake.json'
+import example from './characters/example-characters.json'
 
 // Export as a map
 export const characterModules = {
